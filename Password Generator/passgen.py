1031064d29a8e300d7d84f57b4449c75237d4310
R-C-McDermott <ryan.c.mcdermott4@gmail.com>
2021-07-11 13:36:05 +0100

    Addition of checkboxes
    Adding the option for users to set whether they would like to include numbers and/or punctuation to their password

--- Password Generator/passgen.py
+++ Password Generator/passgen.py
@@ -9,7 +9,10 @@ from tkinter import simpledialog
 import string
 import random
 
-charsPunc = list(string.digits + string.ascii_letters + string.punctuation)
+charsPuncNums = list(string.digits + string.ascii_letters + string.punctuation)
+charsNums = list(string.digits + string.ascii_letters)
+charsPunc = list(string.ascii_letters + string.punctuation)
+chars = list(string.ascii_letters)
 
 class MainApplication(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
@@ -17,29 +20,70 @@ class MainApplication(tk.Frame):
         self.parent = parent
 
         parent.title("Password Generator")
-        parent.geometry("300x150")
+        parent.geometry("300x180")
 
-        self.gen_button = tk.Button(self, text="Generate password", command=self.generatePassword)
-        self.gen_button.pack(side="bottom")
 
-        self.label = tk.Label(parent, text="Input:")
+        # Check box to include punctuation or not in password generation
+        self.punc = tk.IntVar()
+        self.check_box = tk.Checkbutton(parent, text="Include punctuation?", variable=self.punc)
+        self.check_box.pack(side="bottom")
+
+        # Check box to include numbers or not in password generation
+        self.nums = tk.IntVar()
+        self.check_box = tk.Checkbutton(parent, text="Include numbers?", variable=self.nums)
+        self.check_box.pack(side="bottom")
+
+        # Text box for user input to give number of characters wanted
+        self.label = tk.Label(parent, text="Please enter number of characters:")
         self.label.pack(side="top")
         self.user_input_box = tk.Entry(self)
         self.user_input_box.pack(side="top")
 
-        self.outputLab = tk.Label(parent, text="Output: ")
+        # Output text box (copyable text) for generated password
+        self.outputLab = tk.Label(parent, text="Password: ")
         self.outputLab.place(x=125, y=50)
         self.output_text = tk.Text(parent, height=1, width=25)
         self.output_text.place(x=50, y=70)
 
-    # Password generation
+        # Button for generating password
+        self.gen_button = tk.Button(parent, text="Generate password", command=self.generatePassword)
+        self.gen_button.pack(side="bottom")
 
-    def generatePassword(self):
+    # Password generation
+    def passwordPuncNums(self):
+        password = ''
+        size = int(self.user_input_box.get())
+        self.output_text.delete("1.0","end")
+        self.output_text.insert(1.0, password.join(random.sample(charsPuncNums, size)))
+
+    # Password generation without using punctuation
+    def passwordNoPunc(self):
+        password = ''
+        size = int(self.user_input_box.get())
+        self.output_text.delete("1.0","end")
+        self.output_text.insert(1.0, password.join(random.sample(charsNums, size)))
+
+    def passwordNoNums(self):
         password = ''
         size = int(self.user_input_box.get())
         self.output_text.delete("1.0","end")
         self.output_text.insert(1.0, password.join(random.sample(charsPunc, size)))
 
+    def passwordNoNumsNoPunc(self):
+        password = ''
+        size = int(self.user_input_box.get())
+        self.output_text.delete("1.0","end")
+        self.output_text.insert(1.0, password.join(random.sample(chars, size)))
+
+    def generatePassword(self):
+        if self.punc.get() == 1 and self.nums.get() == 1:
+            self.passwordPuncNums()
+        if self.punc.get() == 0 and self.nums.get() == 0:
+            self.passwordNoNumsNoPunc()
+        if self.punc.get() == 1 and self.nums.get() == 0:
+            self.passwordNoNums()
+        if self.punc.get() == 0 and self.nums.get() == 1:
+            self.passwordNoPunc()
 
 if __name__ == "__main__":
     root = tk.Tk()
