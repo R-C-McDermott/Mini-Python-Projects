98bd7bbcdb47491434978799fe98d21805a63078
R-C-McDermott <ryan.c.mcdermott4@gmail.com>
2021-07-14 16:52:12 +0100

    Create cards.py
    Initial code for a potential card game project - maybe blackjack.
    
    Constructed Card, Deck and Player classes.

---
+++ Deck of Cards/cards.py
@@ -0,0 +1,81 @@
+# Building a deck of cards using Loop
+
+import random
+
+suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
+values = [x for x in range(1,14)]
+
+class Card:
+
+    def __init__(self, suit, number):
+
+        self.suit = suit
+        self.number = number
+
+    def changeFormat(self, card_value):
+        if card_value == 1:
+            card_value = "Ace"
+        if card_value == 11:
+            card_value = "Jack"
+        if card_value == 12:
+            card_value = "Queen"
+        if card_value == 13:
+            card_value = "King"
+        self.number = card_value
+
+    def show(self):
+        print(f"{self.number} of {self.suit}")
+
+class Deck:
+
+    def __init__(self):
+        self.cards = []
+        self.buildDeck()
+
+    def deckSize(self):
+        return len(self.cards)
+
+    def buildDeck(self):
+        for i in suits:
+            for j in values:
+                self.cards.append(Card(i, j))
+
+    def printDeck(self):
+        for c in self.cards:
+            c.changeFormat(c.number) # changes 1, 11, 12 and 13 to Ace, Jack, Queen and King
+            c.show()
+
+    def drawCard(self):
+        return self.cards.pop()
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.hand = []
+
+    def draw(self, deck):
+        number_of_cards = 2
+        for i in range(number_of_cards):
+            self.hand.append(deck.drawCard())
+
+    def showHand(self):
+        for h in self.hand:
+            h.show()
+
+def main():
+    # Create Player and Deck
+    p = Player("Ryan")
+    d = Deck()
+
+    # Check deck size
+    print(f"Deck size: {d.deckSize()}")
+
+    # Player draws hand and shows
+    p.draw(d)
+    p.showHand()
+
+    # Check deck size again to ensure everything is working properly
+    print(f"Deck size: {d.deckSize()}")
+
+if __name__ == '__main__':
+    main()
